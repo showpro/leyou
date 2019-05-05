717c17bb68f1416f47d8723e16400a79b4a2d78e
zhan <42181249@qq.com>
2019-05-05 22:12:05 +0800

    商品查询..

--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/impl/SpecificationServiceImpl.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/impl/SpecificationServiceImpl.java
@@ -10,10 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-/**
- * @author zhan
- * @create 2019-05-04-18:18
- */
+
 @Service
 public class SpecificationServiceImpl implements SpecificationService {
 
